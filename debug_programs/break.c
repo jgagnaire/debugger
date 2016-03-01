7adb9ea0d7741cce01c50f4e17d6b86962c72699
chazz <vangysel.charles@gmail.com>
2016-03-01 04:26:44 -0800

    add func and variable

--- debug_programs/break.c
+++ debug_programs/break.c
@@ -1,7 +1,17 @@
 #include <sys/ptrace.h>
 #include <stdio.h>
 
+int         iwanta_locsection() {
+    char    c[] = "putain de bordel de ta race la chauve";
+    return 0;
+}
+
+int         calculate(void) {
+    int     i = 2, j = 2;
+    return i + j;
+}
+
 int         main() {
-    printf("Hello, World !\n");
+    printf("Hello, World ! %d\n", calculate());
     return 0;
 }
